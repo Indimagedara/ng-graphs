a1711e3497a2ec837402b176629c4126a83464d5
nimezzz <nimeshnishara@gmail.com>
2019-11-04 15:55:40 +0530

    Pie chart: added

--- src/app/components/piechart/piechart.component.ts
+++ src/app/components/piechart/piechart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from '../../../assets/js/canvasjs.min';
 
 @Component({
   selector: 'app-piechart',
@@ -10,6 +11,29 @@ export class PiechartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let canvas = new CanvasJS.Chart("chartContainer",{
+      theme: "light2",
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Sample Pie chart"
+      },
+      data: [{
+        type: "pie",
+        showInLegend: true,
+        toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+        indexLabel: "{name} - #percent%",
+        dataPoints: [
+          { y: 450, name: "Food" },
+          { y: 120, name: "Insurance" },
+          { y: 300, name: "Traveling" },
+          { y: 800, name: "Housing" },
+          { y: 150, name: "Education" },
+          { y: 150, name: "Shopping"},
+          { y: 250, name: "Others" }
+        ]
+      }]
+    })
   }
 
 }
