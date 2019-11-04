de3046110f959ce91065dc55ce3b2f386c6cafea
nimezzz <nimeshnishara@gmail.com>
2019-11-04 15:42:54 +0530

    Bar chart: added

--- src/app/components/barchart/barchart.component.ts
+++ src/app/components/barchart/barchart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as CanvasJS from '../../../assets/js/canvasjs.min';
+
 @Component({
   selector: 'app-barchart',
   templateUrl: './barchart.component.html',
@@ -10,6 +12,28 @@ export class BarchartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let chart = new CanvasJS.Chart("chartContainer",{
+      animationEnabled : true,
+      exportEnabled: true,
+      title: {
+        text: "Bar chart in Angular 7"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { y: 71, label: "Apple" },
+          { y: 55, label: "Mango" },
+          { y: 50, label: "Orange" },
+          { y: 65, label: "Banana" },
+          { y: 95, label: "Pineapple" },
+          { y: 68, label: "Pears" },
+          { y: 28, label: "Grapes" },
+          { y: 34, label: "Lychee" },
+          { y: 14, label: "Jackfruit" }
+        ]
+      }]
+    });
+    chart.render();
   }
 
 }
